a854f62ba99f2f588a1ee620a4bd15065b847b0e
lim95 <978656221@qq.comm>
2017-05-24 09:12:27 +0800

    第一次提交，刚搭建好环境。

---
+++ src/main/java/com/yuerbao/gymms/match/action/UserAction.java
@@ -0,0 +1,28 @@
+package com.yuerbao.gymms.match.action;
+
+import com.yuerbao.gymms.match.mapper.UserMapper;
+import com.yuerbao.gymms.match.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by lim9527 on 5/20 0020.
+ */
+@Controller
+@RequestMapping(value = "/match")
+public class UserAction {
+
+    @Autowired
+    UserMapper userMapper;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String test(){
+
+        User user = userMapper.findUserById(1);
+        if (user != null)
+            System.out.println(user);
+        return "/match/match_index";
+    }
+}
---
+++ src/main/webapp/resource/js/gymms-common.js
@@ -0,0 +1,7 @@
+
+$(function() {
+    $("#header").load("resource/html/gymms_header.html .container");
+	$("#footer").load("resource/html/gymms_footer.html .container");
+});
+
+// alert("gymms has been load");

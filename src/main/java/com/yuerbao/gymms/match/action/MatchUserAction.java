fbebf4bd80c943e73c69eba964ed906e9db4b287
lim95 <978656221@qq.comm>
2017-05-24 23:19:58 +0800

    同一份html代码，match所在的路径不能正常显示，提交后会恢复到上一次。

---
+++ src/main/java/com/yuerbao/gymms/match/action/MatchUserAction.java
@@ -0,0 +1,31 @@
+package com.yuerbao.gymms.match.action;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by lim9527 on 5/24 0024.
+ */
+@Controller
+@RequestMapping("/match/user")
+public class MatchUserAction {
+
+    @RequestMapping(path = "/bookingItemTable", method = RequestMethod.GET)
+    public String write(){
+        return "match/match_booking_item_table_user";
+    }
+
+//    @ResponseBody
+//    @RequestMapping(path = "/bookingItemTableAjax", method = RequestMethod.GET)
+//    public String jsonTest(){
+//        String json = "{\"name\":\"test\",\"num\":\"3\",\"info\":[" +
+//                "{\"id\":\"20161024\",\"user\":\"周杰伦\",\"match\":\"魔杰座-2016校园巡回演唱会湛江站\",\"status\":\"申请中\",\"url\":\"www.baidu.com\"}," +
+//                "{\"id\":\"20161025\",\"user\":\"周杰伦\",\"match\":\"魔杰座-2016校园巡回演唱会湛江站\",\"status\":\"申请中\",\"url\":\"www.baidu.com\"}," +
+//                "{\"id\":\"20161026\",\"user\":\"周杰伦\",\"match\":\"魔杰座-2016校园巡回演唱会湛江站\",\"status\":\"申请中\",\"url\":\"www.baidu.com\"}," +
+//                "]}";
+//        System.out.println(json);
+//        return json;
+//    }
+}
